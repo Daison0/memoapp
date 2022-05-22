d3460ad2d4dd55a3223a879228bd14c08d550bb6
EC2 Default User <ec2-user@ip-172-31-5-96.us-west-2.compute.internal>
2022-05-22 08:10:13 +0000

    memoapp

---
+++ memo.rb
@@ -0,0 +1,34 @@
+require "csv"
+puts "1(新規でメモを作成)2(既存のメモを編集)"
+
+memo_type = gets.to_i
+
+if memo_type == 1
+
+    puts "拡張子を除いたファイル名を記入してください"
+    file_name = gets.chomp
+    puts "メモの内容を記入してください"
+    puts "完了したらCtrl+Dを押します"
+    str = STDIN.gets
+    memo = str
+    File.open("#{file_name}.csv","w") do |csv|
+    csv.puts ["#{memo}"]
+        
+    end
+
+
+end
+
+if memo_type == 2
+    
+    puts "拡張子を除いたファイル名を記入してください"
+    file_name = gets.chomp.to_s
+    puts "編集内容を入力して下さい"
+    puts "完了したらコントロールDを押します"
+    input = $stdin.read
+    CSV.open("#{file_name}.csv","a") do |csv|
+    csv.puts [input]
+        
+    end
+    
+end
